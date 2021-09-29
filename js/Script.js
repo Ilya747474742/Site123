e4044eac112e6b737948ad75e8b8d37c2b87a652
Ilya747474742 <68121715+Ilya747474742@users.noreply.github.com>
2021-09-29 11:50:13 +0700

    new site

---
+++ js/Script.js
@@ -0,0 +1,10 @@
+﻿window.onload = function menuF() {
+    var menu = document.getElementById('myTopnav');
+    menu.onclick = function myFunction() {
+        if (menu.className === 'topnav') {
+            menu.className += ' responsive';
+        } else {
+            menu.className = 'topnav';
+        }
+    }
+}
